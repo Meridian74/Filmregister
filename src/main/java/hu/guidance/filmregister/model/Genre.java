b4ccf827108f8aebc81e58f58d45e4ff18f78ff9
DESKTOP-2TO07RP\laszlo.fazekas <laszlo.fazekas@guidance.hu>
2022-02-10 17:23:55 +0100

    Create entity class Genre

---
+++ src/main/java/hu/guidance/filmregister/model/Genre.java
@@ -0,0 +1,24 @@
+package hu.guidance.filmregister.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "GENRES")
+public class Genre {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+}
