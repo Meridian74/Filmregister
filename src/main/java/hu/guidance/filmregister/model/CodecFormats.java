c9fb4d62e2991997208305beeab0941a6f6cb0a8
DESKTOP-2TO07RP\laszlo.fazekas <laszlo.fazekas@guidance.hu>
2022-02-11 09:27:56 +0100

    Create entity class CodecFormats

---
+++ src/main/java/hu/guidance/filmregister/model/CodecFormats.java
@@ -0,0 +1,26 @@
+package hu.guidance.filmregister.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "CODEC_FORMATS")
+public class CodecFormats {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "ID")
+    private long id;
+
+    @Column(name = "NAME")
+    private String name;
+
+}
