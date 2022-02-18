e9a59c60f35fbe58e082e710f25dc057bb74d2b1
DESKTOP-2TO07RP\laszlo.fazekas <laszlo.fazekas@guidance.hu>
2022-02-18 11:08:48 +0100

    Implement MovieDurationConverter test

--- src/main/java/hu/guidance/filmregister/model/MovieDurationConverter.java
+++ src/main/java/hu/guidance/filmregister/model/MovieDurationConverter.java
@@ -11,27 +11,40 @@ public class MovieDurationConverter implements AttributeConverter<String, Intege
         if (duration == null || duration.isEmpty() || duration.isBlank()) {
             return null;
         }
+        if (duration.contains("-")) {
+            return null;
+        }
 
         String[] elements = duration.split(":");
         if (elements.length < 3) {
             return null;
         }
 
-        int result = Integer.parseInt(elements[2]) * 3600;
-        result += Integer.parseInt(elements[1]) * 60;
-        result += Integer.parseInt(elements[0]);
-        return result;
+        int secondsOfHours = Integer.parseInt(elements[0]) * 3600;
+        int secondsOfMinutes = Integer.parseInt(elements[1]) * 60;
+        int seconds = Integer.parseInt(elements[2]);
+
+        if(secondsOfMinutes > 3599 || seconds > 59) {
+            return null;
+        }
+
+        return secondsOfHours + secondsOfMinutes + seconds;
     }
 
     @Override
     public String convertToEntityAttribute(Integer duration) {
-        if (duration == null || duration < 1) {
+        if (duration == null || duration < 0) {
             return null;
         }
 
-        int hours = duration / 3600;
-        int minutes = duration % 3600;
-        int seconds = duration % 60;
+        String hours = String.valueOf(duration / 3600);
+        String minutes = String.valueOf(duration / 60 % 60);
+        String seconds = String.valueOf(duration % 60);
+
+
+        if (minutes.equals("0")) minutes = "00";
+        if (seconds.equals("0")) seconds = "00";
+
         return hours + ":" + minutes + ":" + seconds;
     }
 
---
+++ src/test/java/hu/guidance/filmregister/model/MovieDurationConverterTest.java
@@ -0,0 +1,86 @@
+package hu.guidance.filmregister.model;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.params.provider.Arguments.arguments;
+
+class MovieDurationConverterTest {
+
+    static Stream<Arguments> secondsToTimeCode() {
+        return Stream.of(
+                arguments(0, "0:00:00"),
+                arguments(12896, "3:34:56"),
+                arguments(264896, "73:34:56")
+        );
+    }
+
+    static Stream<Arguments> wrongTimeToNull() {
+        return Stream.of(
+                arguments("-1:00:00"),
+                arguments("1:60:12"),
+                arguments("1:12:60")
+        );
+    }
+
+    static Stream<Arguments> timeCodeToSeconds() {
+        return Stream.of(
+                arguments("0:00:00", 0),
+                arguments("3:34:56", 12896),
+                arguments("73:34:56", 264896)
+        );
+    }
+
+    private static final MovieDurationConverter MDC = new MovieDurationConverter();
+
+
+    @ParameterizedTest
+    @MethodSource("secondsToTimeCode")
+    void durationSecondsConvertTimeCode(Integer expectedSeconds, String timeCode) {
+        Integer time = MDC.convertToDatabaseColumn(timeCode);
+        assertEquals(expectedSeconds, time);
+    }
+
+    @ParameterizedTest
+    @MethodSource("wrongTimeToNull")
+    void wrongTimeCodeToNull(String wrongTimeCode) {
+        Integer time = MDC.convertToDatabaseColumn(wrongTimeCode);
+        assertNull(time);
+    }
+
+    @Test
+    void durationSecondsConvertTimeCode() {
+        String timeCode = null;
+        Integer time = MDC.convertToDatabaseColumn(timeCode);
+        assertNull(time);
+    }
+
+
+    @ParameterizedTest
+    @MethodSource("timeCodeToSeconds")
+    void durationTimeCodeConvertSeconds(String expectedTimeCode, Integer time) {
+        String timeCode = MDC.convertToEntityAttribute(time);
+        assertEquals(expectedTimeCode, timeCode);
+    }
+
+    @Test
+    void durationTimeCodeConvertSecondsTestNegative() {
+        Integer time = -1;
+        String timeCode = MDC.convertToEntityAttribute(time);
+        assertNull(timeCode);
+    }
+
+    @Test
+    void durationTimeCodeConvertSecondsTestNull() {
+        Integer time = null;
+        String timeCode = MDC.convertToEntityAttribute(time);
+        assertNull(timeCode);
+    }
+
+}
